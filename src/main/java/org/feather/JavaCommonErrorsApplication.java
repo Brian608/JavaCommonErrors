c7c6f16fceb9d4aa146c6a3e0b4d7d4f13dab77b
feather <Brian608@163.com>
2022-02-01 21:15:26 +0800

    project init

---
+++ src/main/java/org/feather/JavaCommonErrorsApplication.java
@@ -0,0 +1,17 @@
+package org.feather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: javaCommonErrors
+ * @description:
+ * @author: 杜雪松(feather)
+ * @since: 2022-02-01 21:04
+ **/
+@SpringBootApplication
+public class JavaCommonErrorsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCommonErrorsApplication.class,args);
+    }
+}
---
+++ src/test/java/org/feather/javacommonerrors/JavaCommonErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.feather.javacommonerrors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCommonErrorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
